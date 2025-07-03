42d5ceca6c5292e290ec2462d0fc5d43fbbb9162
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-07-03 09:29:29 -0500

    servicio data creado con JsonPlaceHolder - interfacae Post creada - actualiza config para usar http

---
+++ dreamapp/src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from './post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+}
